082343b00281eacdb8899a7df3f598c855ff6a9b
voicustefan <voicu.stefan@courrier.uqam.ca>
2022-10-27 14:33:44 -0400

    blank login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   #get 'home/index'
   root "home#index"
-  get 'home/corporate'
-  get 'home/residential'
-  get 'home/form'
+  get 'corporate', to:'home#corporate'
+  get 'residential', to: 'home#residential'
+  get 'form', to: 'home#form'
+  get 'login', to: 'home#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
