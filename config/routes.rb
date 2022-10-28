b2f19e2189297c70a735525abd4280306581e126
voicustefan <voicu.stefan@courrier.uqam.ca>
2022-10-28 12:49:54 -0400

    navbar connects to admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'residential', to: 'home#residential'
   get 'form', to: 'home#form'
   get 'login', to: 'home#login'
+  get 'admin', to: 'home#admin'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
