69b4f8461a168bc81062acdade9d344e88459d6d
lonnimoss <gloryjacques1@gmail.com>
2022-10-28 11:53:02 -0400

    set links to corp, resi and index pages (navbar + button)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'home/residential'
   get 'home/form'
 
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
