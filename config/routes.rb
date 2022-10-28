8b68577395e9f73259dc6c913044ceb8daef817a
yasmi66 <yasmina.suwed@outlook.com>
2022-10-28 19:56:47 -0400

    test push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'quotes/requestQuote'
+  post '/request_quote', to: 'quotes#create'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   #get 'home/index'
   root "home#index"
+  get 'home/corporate'
+  get 'home/residential'
+  get 'home/form'
   resources :quotes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
