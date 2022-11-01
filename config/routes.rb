fb7048ced77c70a30c5c46a8e8086fef4ebc3199
10gianluca <gciccone01@gmail.com>
2022-11-01 11:36:29 -0400

    wtf why no work

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,6 @@ Rails.application.routes.draw do
     get 'home/admin'
 
   resources :quotes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL this file, see http://guides.rubyonrails.org/routing.html
 
 end
