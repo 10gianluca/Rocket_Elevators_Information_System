c366a1c91d164450251841f84b587cea5a925334
ChefAdi <adityamenonchef@gmail.com>
2022-11-02 10:04:32 -0400

    lol added seed

--- db/migrate/20221031171722_create_elevators.rb
+++ db/migrate/20221031171722_create_elevators.rb
@@ -1,10 +1,10 @@
 class CreateElevators < ActiveRecord::Migration[5.2]
   def change
     create_table :elevators do |t|
-      t.integer :columnID
+      t.string :columnID
       t.string :serialNumber
-      t.boolean :model
-      t.boolean :type
+      t.string :model
+      t.string :elevatorType
       t.string :status
       t.datetime :commissioningDate
       t.datetime :lastInspection
