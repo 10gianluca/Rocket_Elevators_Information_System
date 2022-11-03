03b144125506ec72969e9aa969001a288ed797d8
lonnimoss <gloryjacques1@gmail.com>
2022-11-03 16:23:22 -0400

    db_rake update, in lib folder

--- config/initializers/eg.rb
+++ config/initializers/eg.rb
@@ -1,1 +1,1 @@
-POSTGRES_DATABASE = YAML::load( ERB.new( File.read("#{ Rails.root }/config/database.postgres.yml") ).result )[Rails.env.to_s]
+# POSTGRES_DATABASE = YAML::load( ERB.new( File.read("#{ Rails.root }/config/database.postgres.yml") ).result )[Rails.env.to_s]
