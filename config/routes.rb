95d4c4eea217203331e2b5ee0aa3006adeb726eb
voicustefan <voicu.stefan@courrier.uqam.ca>
2022-11-03 15:04:12 -0400

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
   get 'quotes/requestQuote'
   post '/request_quote', to: 'quotes#create'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # get 'contact/_contact_us', to: 'contact#_contact_us', as: 'new_contact'
-  #get 'home/index'
     root "home#index"
     get 'home/corporate'
     get 'home/residential'
@@ -22,7 +20,6 @@ Rails.application.routes.draw do
     get 'home/login'
     get 'home/admin'
   resources :quotes
-  post '/contacts', to: 'home#create'
   # For details on the DSL this file, see http://guides.rubyonrails.org/routing.html
 
 end
