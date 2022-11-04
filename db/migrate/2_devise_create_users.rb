78a6d944e8a82a882d4f4cdd0c5eab52680feb2b
10gianluca <gciccone01@gmail.com>
2022-11-04 14:53:59 -0400

    rake file update v.5

--- db/migrate/2_devise_create_users.rb
+++ db/migrate/2_devise_create_users.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     def change
+      drop_table :users
       create_table :users do |t|
         t.string :title
         t.string :first_name
